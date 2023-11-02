94292630802e9a35bc2ab2b3adc3207b2cb8326d
Mmtzatzyy <114859674+Mmtzatzyy@users.noreply.github.com>
2023-11-03 01:18:15 +0700

    Templating backend

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('frontend.index');
 });
+
+Route::get('/admin', function () {
+    return view('backend.index');
+});
