60cc3cd20e71f452e6be5760abf7b1c4c90f3562
erickdb <erickdarmawanboeniarto130803@gmail.com>
2023-11-12 05:20:36 +0700

    style: styling login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,24 +24,40 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
+        <!-- Remember Me And Forgot Password -->
         <div class="block mt-4">
+            <div class="flex items-center justify-between">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
+        </div>
+        </div>
 
-            <x-primary-button class="ms-3">
+        <div class="flex items-center justify-end mt-4">
+            <x-primary-button class="">
                 {{ __('Log in') }}
             </x-primary-button>
+        </div>
+
+        <!-- Remember Me And Forgot Password -->
+        <div class="block mt-4">
+            <div class="flex items-center justify-between">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ms-2 text-sm text-gray-600">{{ __('Register') }}</span>
+            </label>
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Back To Portal Berita') }}
+                </a>
+            @endif
+        </div>
         </div>
     </form>
 </x-guest-layout>
