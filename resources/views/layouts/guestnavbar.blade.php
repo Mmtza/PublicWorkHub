5c0ca0e14d829033d3a0369645327e2c4d879388
Anuraaaa <anwarfauzan75@gmail.com>
2023-11-13 09:44:19 +0700

    style/auth: styling auth

---
+++ resources/views/layouts/guestnavbar.blade.php
@@ -0,0 +1,22 @@
+<div class="site-mobile-menu site-navbar-target">
+    <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close">
+            <span class="icofont-close js-menu-toggle"></span>
+        </div>
+    </div>
+    <div class="site-mobile-menu-body"></div>
+</div>
+
+<nav class="site-nav">
+    <div class="container">
+        <div class="menu-bg-wrap">
+            <div class="site-navigation">
+                <div class="row g-0 align-items-center">
+                    <div class="col-8">
+                        <a href={{ route('landing') }} class="logo m-0 float-start">Public Work Hub<span
+                                class="text-primary">.</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+</nav>
