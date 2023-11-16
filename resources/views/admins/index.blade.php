7795ad9e3a0938e2d77d623c584aac22c0b41633
erickdb <erickdarmawanboeniarto130803@gmail.com>
2023-11-16 16:23:03 +0700

    style/admin : change style for dashboard admin

--- resources/views/admins/index.blade.php
+++ resources/views/admins/index.blade.php
@@ -9,8 +9,8 @@
           <div class="row g-4">
             <div class="col-12 col-xxl-6">
               <div class="mb-8">
-                <h2 class="mb-2">Ecommerce Dashboard</h2>
-                <h5 class="text-700 fw-semi-bold">Here’s what’s going on at your business right now</h5>
+                <h2 class="mb-2">Dashboard</h2>
+                <h5 class="text-700 fw-semi-bold">Here’s what’s going on at publick work hub right now</h5>
               </div>
               <div class="row align-items-center g-4">
                 <div class="col-12 col-md-auto">
@@ -74,11 +74,11 @@
                       <div class="d-flex justify-content-between">
                         <div>
                           <h5 class="mb-1">
-                            Total orders<span class="badge badge-phoenix badge-phoenix-warning rounded-pill fs--1 ms-2"><span class="badge-label">-6.8%</span></span>
+                            Total Pengaduan<span class="badge badge-phoenix badge-phoenix-warning rounded-pill fs--1 ms-2"><span class="badge-label">-6.8%</span></span>
                           </h5>
                           <h6 class="text-700">Last 7 days</h6>
                         </div>
-                        <h4>16,247</h4>
+                        <h4>247</h4>
                       </div>
                       <div class="d-flex justify-content-center px-4 py-6">
                         <div class="echart-total-orders" style="height: 85px; width: 115px"></div>
@@ -86,13 +86,18 @@
                       <div class="mt-2">
                         <div class="d-flex align-items-center mb-2">
                           <div class="bullet-item bg-primary me-2"></div>
-                          <h6 class="text-900 fw-semi-bold flex-1 mb-0">Completed</h6>
-                          <h6 class="text-900 fw-semi-bold mb-0">52%</h6>
+                          <h6 class="text-900 fw-semi-bold flex-1 mb-0">Menunggu</h6>
+                          <h6 class="text-900 fw-semi-bold mb-0">25%</h6>
                         </div>
                         <div class="d-flex align-items-center">
                           <div class="bullet-item bg-primary-100 me-2"></div>
-                          <h6 class="text-900 fw-semi-bold flex-1 mb-0">Pending payment</h6>
-                          <h6 class="text-900 fw-semi-bold mb-0">48%</h6>
+                          <h6 class="text-900 fw-semi-bold flex-1 mb-0">Di terima</h6>
+                          <h6 class="text-900 fw-semi-bold mb-0">25%</h6>
+                        </div>
+                        <div class="d-flex align-items-center mt-2">
+                          <div class="bullet-item bg-info-500 me-2"></div>
+                          <h6 class="text-900 fw-semi-bold flex-1 mb-0">Di tolak</h6>
+                          <h6 class="text-900 fw-semi-bold mb-0">50%</h6>
                         </div>
                       </div>
                     </div>
@@ -104,11 +109,11 @@
                       <div class="d-flex justify-content-between">
                         <div>
                           <h5 class="mb-1">
-                            New customers<span class="badge badge-phoenix badge-phoenix-warning rounded-pill fs--1 ms-2"> <span class="badge-label">+26.5%</span></span>
+                            New Users<span class="badge badge-phoenix badge-phoenix-warning rounded-pill fs--1 ms-2"> <span class="badge-label">+26.5%</span></span>
                           </h5>
                           <h6 class="text-700">Last 7 days</h6>
                         </div>
-                        <h4>356</h4>
+                        <h4>1227</h4>
                       </div>
                       <div class="pb-0 pt-4">
                         <div class="echarts-new-customers" style="height: 180px; width: 100%"></div>
@@ -121,28 +126,26 @@
                     <div class="card-body">
                       <div class="d-flex justify-content-between">
                         <div>
-                          <h5 class="mb-2">Top coupons</h5>
+                          <h5 class="mb-1">
+                            Total Berita<span class="badge badge-phoenix badge-phoenix-warning rounded-pill fs--1 ms-2"><span class="badge-label">-6.8%</span></span>
+                          </h5>
                           <h6 class="text-700">Last 7 days</h6>
                         </div>
+                        <h4>247</h4>
                       </div>
-                      <div class="pb-4 pt-3">
-                        <div class="echart-top-coupons" style="height: 115px; width: 100%"></div>
+                      <div class="d-flex justify-content-center px-4 py-6">
+                        <div class="echart-total-orders" style="height: 85px; width: 115px"></div>
                       </div>
-                      <div>
+                      <div class="mt-2">
                         <div class="d-flex align-items-center mb-2">
                           <div class="bullet-item bg-primary me-2"></div>
-                          <h6 class="text-900 fw-semi-bold flex-1 mb-0">Percentage discount</h6>
-                          <h6 class="text-900 fw-semi-bold mb-0">72%</h6>
-                        </div>
-                        <div class="d-flex align-items-center mb-2">
-                          <div class="bullet-item bg-primary-200 me-2"></div>
-                          <h6 class="text-900 fw-semi-bold flex-1 mb-0">Fixed card discount</h6>
-                          <h6 class="text-900 fw-semi-bold mb-0">18%</h6>
+                          <h6 class="text-900 fw-semi-bold flex-1 mb-0">Aktif</h6>
+                          <h6 class="text-900 fw-semi-bold mb-0">50%</h6>
                         </div>
                         <div class="d-flex align-items-center">
-                          <div class="bullet-item bg-info-500 me-2"></div>
-                          <h6 class="text-900 fw-semi-bold flex-1 mb-0">Fixed product discount</h6>
-                          <h6 class="text-900 fw-semi-bold mb-0">10%</h6>
+                          <div class="bullet-item bg-primary-100 me-2"></div>
+                          <h6 class="text-900 fw-semi-bold flex-1 mb-0">Tidak Aktif</h6>
+                          <h6 class="text-900 fw-semi-bold mb-0">50%</h6>
                         </div>
                       </div>
                     </div>
@@ -150,27 +153,18 @@
                 </div>
                 <div class="col-12 col-md-6">
                   <div class="card h-100">
-                    <div class="card-body d-flex flex-column">
+                    <div class="card-body">
                       <div class="d-flex justify-content-between">
                         <div>
-                          <h5 class="mb-2">Paying vs non paying</h5>
+                          <h5 class="mb-1">
+                            New Loker<span class="badge badge-phoenix badge-phoenix-warning rounded-pill fs--1 ms-2"> <span class="badge-label">+26.5%</span></span>
+                          </h5>
                           <h6 class="text-700">Last 7 days</h6>
                         </div>
+                        <h4>1227</h4>
                       </div>
-                      <div class="d-flex justify-content-center pt-3 flex-1">
-                        <div class="echarts-paying-customer-chart" style="height: 100%; width: 100%"></div>
-                      </div>
-                      <div class="mt-3">
-                        <div class="d-flex align-items-center mb-2">
-                          <div class="bullet-item bg-primary me-2"></div>
-                          <h6 class="text-900 fw-semi-bold flex-1 mb-0">Paying customer</h6>
-                          <h6 class="text-900 fw-semi-bold mb-0">30%</h6>
-                        </div>
-                        <div class="d-flex align-items-center">
-                          <div class="bullet-item bg-primary-100 me-2"></div>
-                          <h6 class="text-900 fw-semi-bold flex-1 mb-0">Non-paying customer</h6>
-                          <h6 class="text-900 fw-semi-bold mb-0">70%</h6>
-                        </div>
+                      <div class="pb-0 pt-4">
+                        <div class="echarts-new-customers" style="height: 180px; width: 100%"></div>
                       </div>
                     </div>
                   </div>
@@ -179,7 +173,7 @@
             </div>
           </div>
         </div>
-        <div class="mx-n4 px-4 mx-lg-n6 px-lg-6 bg-white pt-7 border-y border-300">
+        {{-- <div class="mx-n4 px-4 mx-lg-n6 px-lg-6 bg-white pt-7 border-y border-300">
           <div data-list='{"valueNames":["product","customer","rating","review","time"],"page":6}'>
             <div class="row align-items-end justify-content-between pb-5 g-3">
               <div class="col-auto">
@@ -1485,7 +1479,7 @@
               <div class="echart-returning-customer" style="height: 300px"></div>
             </div>
           </div>
-        </div>
+        </div>--}}
     </div>
 </main>
 @endSection
