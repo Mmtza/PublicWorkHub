f22dd366c42a4f0f1835c85eacc9114518f627e3
erickdb <erickdarmawanboeniarto130803@gmail.com>
2023-11-18 22:19:31 +0700

    frontend/admin/users : feat add tambah pengaduan end edit pengaduan

---
+++ resources/views/admins/pages/form_pengaduan/tambah_pengaduan.blade.php
@@ -0,0 +1,39 @@
+@extends('admins.layout.template')
+
+@section('title', 'Management Pengaduan')
+
+@section('content')
+
+<h1 class="fs-1 mb-5">Tambah Pengaduan</h1>
+<form class="d-flex mb-3" method="POST">
+</form>  
+<form class="form-control row g-2 py-3" method="POST" enctype="multipart/form-data" action={{ route('admin.pengaduan.tambah.post') }}>
+    @csrf
+    @method('POST')
+
+    <x-input-label for="nama" :value="('Nama')"/>
+    <x-text-input type="text" class="rounded p-2" name="nama" id="nama"/>
+    
+    <x-input-label for="isi_pengaduan" :value="('Isi Pengaduan')"/>
+    <x-text-input type="text" class="rounded p-2" name="email"/>
+
+    <x-input-label for="tanggal_pengaduan" :value="('Tanggal Pengaduan')"/>
+    <x-text-input type="date" class="rounded p-2" name="alamat"/>
+    
+    <x-input-label for="role" :value="('status')"/>
+    <select name="status" id="status_berita" class="rounded p-2">
+        <option value="menunggu">Menunggu</option>
+        <option value="diteirma">Diterima</option>
+        <option value="ditolak">Ditolak</option>
+    </select>
+    
+    <x-input-label for="image_pengaduan" :value="('Image')"/>
+    <input type="file" accept=".jpg, .png, .jpeg" class="rounded p-2" name="image_berita" id="image_berita"/>
+    
+    <div class="d-flex justify-content-center gap-5">
+        <button type="submit" class="btn btn-primary p-3">{{ __('Tambah') }}</button>
+        <button type="reset" class="btn btn-secondary p-3">{{ __('Reset') }}</button>
+    </div>
+</form>
+
+@endSection
