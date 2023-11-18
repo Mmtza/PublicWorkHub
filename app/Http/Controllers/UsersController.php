c724343c07fdb59722e5fdf19133d0e4f1c30352
erickdb <erickdarmawanboeniarto130803@gmail.com>
2023-11-18 22:21:40 +0700

    frontend/admin/users and pengaduan : add rout with controller for pengaduan and users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,4 +10,20 @@ class UsersController extends Controller
     {
         return view('admins.pages.form_user.user');
     }
+
+    public function viewEditUsers() {
+        return view('admins.pages.form_user.edit_user');
+    }
+
+    public function viewAddUsers() {
+        return view('admins.pages.form_user.tambah_users');
+    }
+
+    public function editUsers() {
+
+    }
+
+    public function addUsers() {
+
+    }
 }
