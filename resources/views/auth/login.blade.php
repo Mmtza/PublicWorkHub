4cc587d8c609f90b5679bfd0ece732530afa0a1e
Mmtza <sinkaiytb@gmail.com>
2023-11-25 15:47:04 +0700

    frontend/login: add loading on login pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,19 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-2" :status="session('status')" />
-    
+
     <form method="POST" action="{{ route('login') }}">
         @csrf
-        <x-custom-logo/>
+        <x-custom-logo />
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
             <label for="remember_me" class="inline-flex items-center mt-2">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <input id="remember_me" type="checkbox"
+                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
         </div>
@@ -20,20 +22,20 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
             <div class="flex justify-between mt-2">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
                 @if (Route::has('landing'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('landing') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        href="{{ route('landing') }}">
                         {{ __('Back to home') }}
                     </a>
                 @endif
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,39 +1,62 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <link rel="stylesheet" href="{{ asset('users/themes') }}/fonts/icomoon/style.css">
-        <link rel="stylesheet" href="{{ asset('users/themes') }}/fonts/flaticon/font/flaticon.css">
-    
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    
-        <link rel="stylesheet" href="{{ asset('users/themes') }}/css/tiny-slider.css">
-        <link rel="stylesheet" href="{{ asset('users/themes') }}/css/aos.css">
-        <link rel="stylesheet" href="{{ asset('users/themes') }}/css/glightbox.min.css">
-        <link rel="stylesheet" href="{{ asset('users/themes') }}/css/style.css">
-    
-        <link rel="stylesheet" href="{{ asset('users/themes') }}/css/flatpickr.min.css">        
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        @include('layouts.guestnavbar')
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+    <link rel="stylesheet" href="{{ asset('users/themes') }}/fonts/icomoon/style.css">
+    <link rel="stylesheet" href="{{ asset('users/themes') }}/fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="{{ asset('users/themes') }}/css/tiny-slider.css">
+    <link rel="stylesheet" href="{{ asset('users/themes') }}/css/aos.css">
+    <link rel="stylesheet" href="{{ asset('users/themes') }}/css/glightbox.min.css">
+    <link rel="stylesheet" href="{{ asset('users/themes') }}/css/style.css">
+
+    <link rel="stylesheet" href="{{ asset('users/themes') }}/css/flatpickr.min.css">
+</head>
+
+<body class="font-sans text-gray-900 antialiased">
+    @include('layouts.guestnavbar')
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+
+
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+            <!-- Preloader -->
+            <div id="overlayer"></div>
+            <div class="loader">
+                <div class="spinner-border text-primary" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
             </div>
+            <!-- Session Status -->
         </div>
-    </body>
+    </div>
+
+    <script src="{{ asset('users/themes') }}/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('users/themes') }}/js/tiny-slider.js"></script>
+
+    <script src="{{ asset('users/themes') }}/js/flatpickr.min.js"></script>
+
+
+    <script src="{{ asset('users/themes') }}/js/aos.js"></script>
+    <script src="{{ asset('users/themes') }}/js/glightbox.min.js"></script>
+    <script src="{{ asset('users/themes') }}/js/navbar.js"></script>
+    <script src="{{ asset('users/themes') }}/js/counter.js"></script>
+    <script src="{{ asset('users/themes') }}/js/custom.js"></script>
+</body>
+
 </html>
