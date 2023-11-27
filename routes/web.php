3d775279aa5609047e217c408bbb2c92b7fe3cec
Mmtza <sinkaiytb@gmail.com>
2023-11-28 03:34:48 +0700

    backend/pengaduan: add route delete pengaduan

--- routes/web.php
+++ routes/web.php
@@ -84,9 +84,11 @@ Route::middleware(['auth', 'Admin'])->group(function () {
 
     Route::get('/admin/pengaduan/tambah', [PengaduanController::class, 'viewAddPengaduan'])->name('admin.pengaduan.tambah');
 
-    Route::post('/admin/pengaduan/tambah', [PengaduanController::class, 'addPengaduan'])->name('admin.pengaduan.tambah.post');
+    Route::post('/admin/pengaduan/tambah', [PengaduanController::class, 'addPengaduanDashboard'])->name('admin.pengaduan.tambah.post');
 
     Route::patch('/admin/pengaduan/edit/{id}', [PengaduanController::class, 'editPengaduanDashboard'])->name('admin.pengaduan.edit.patch');
+
+    Route::delete('/admin/pengaduan/delete/{id}', [PengaduanController::class, 'deletePengaduanDashboard'])->name('admin.pengaduan.delete');
 
     Route::get('/admin/loker', [LokerController::class, 'showAllLokerDashboard'])->name('admin.loker');
 
