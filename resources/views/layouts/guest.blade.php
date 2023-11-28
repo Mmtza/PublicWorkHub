1f14bed252f6459d7f5215cce9d82f6de1f20777
Mmtza <sinkaiytb@gmail.com>
2023-11-28 23:56:56 +0700

    add icon to browser tab

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>PWH | Admin</title>
+
+    {{-- Icon web --}}
+    <link rel="icon" href="{{ asset('users/themes') }}/images/pwhlogo3.png" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
