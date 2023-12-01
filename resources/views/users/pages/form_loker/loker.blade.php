6d90bb506d0dd8823f7f4e61d9fdf1102207697b
erickdb <erickdarmawanboeniarto130803@gmail.com>
2023-12-01 22:04:47 +0700

    add pages loker in user and their controller

---
+++ resources/views/users/pages/form_loker/loker.blade.php
@@ -0,0 +1,98 @@
+@extends('users.layout.template')
+
+@section('title', 'Loker')
+
+@section('content')
+    <!-- Start retroy layout blog posts -->
+    <section class="section bg-light">
+        <div class="container">
+            <div class="row align-items-stretch retro-layout">
+                <div class="col-md-3 order-last order-lg-first">
+                    <h1 class="text-dark mt-3 ms-4"><b>Daftar Loker</b></h1>
+                        <ul class="list-unstyled">
+                            <li class="mb-2 p-0 border border-primary border-1 custom-card-hover shadow">
+                                <a href="" class="text-decoration-none">
+                                    <div class="d-flex">
+                                        <img width="30%" src="{{ asset('users/themes') }}/images/pwhlogo3.png" alt="">
+                                        <div class="card w-16" style="width: 18rem;">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Frontend Web Development</h5>
+                                                <span class="card-text">Pt . Public Work Hub</span><br>
+                                                <span class="card-text">Jl.Jendral Ahmad Yani</span><br>
+                                                <Span class="card-text">11-12-2023</Span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li class="mb-2 p-0 border border-primary border-1 custom-card-hover shadow">
+                                <a href="" class="text-decoration-none">
+                                    <div class="d-flex">
+                                        <img width="30%" src="{{ asset('users/themes') }}/images/pwhlogo3.png" alt="">
+                                        <div class="card w-16" style="width: 18rem;">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Frontend Web Development</h5>
+                                                <span class="card-text">Pt . Public Work Hub</span><br>
+                                                <span class="card-text">Jl.Jendral Ahmad Yani</span><br>
+                                                <Span class="card-text">11-12-2023</Span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li class="mb-2 p-0 border border-primary border-1 custom-card-hover shadow">
+                                <a href="" class="text-decoration-none">
+                                    <div class="d-flex">
+                                        <img width="30%" src="{{ asset('users/themes') }}/images/pwhlogo3.png" alt="">
+                                        <div class="card w-16" style="width: 18rem;">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Frontend Web Development</h5>
+                                                <span class="card-text">Pt . Public Work Hub</span><br>
+                                                <span class="card-text">Jl.Jendral Ahmad Yani</span><br>
+                                                <Span class="card-text">11-12-2023</Span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                </div>
+                <div class="col-md-9 order-first order-lg-last mt-lg-4">
+                    <h2 class="text-dark"><b>Frontend Web Development</b></h2>
+                    <p>Jl.Jendral Ahmad Yani</p>
+                    <p>Pt . Public Work Hub</p>
+                    <hr>
+                    <div class="d-flex mb-3">
+                        <a href="" class="btn btn-outline-primary rounded">Daftar</a>
+                    </div>
+                    <div class="berita-isi-wrapper">
+                        Social Media Admin <br>
+
+                        <br>
+                        Placement in Alam Sutera (On-site) <br>
+
+                        <br>
+                        About Super <br>
+                        <br>
+
+                        We are a group of business enthusiasts, scientists, communicators, designers, productive mothers, and many more who are unified and unduly spirited to work towards one big vision and goal. As a leading social commerce platform in Indonesia, we aim to solve economic inequality distribution across rural areas by building agent networks and hyperlocal supply chains to make goods more affordable. Aplikasi Super goes beyond passion and creativity to decipher social issues in our surroundings. <br>
+
+                        <br>
+                        Key Responsibilities: <br>
+                        • Develop creative and engaging content for social media platforms. <br>
+                        • Schedule and publish content on various social media channels. <br>
+                        • Interact with the audience, respond to comments, and foster community engagement. <br>
+                        • Monitor social media analytics, track performance, and provide reports on key metrics. <br>
+
+                        <br>
+                        Role Requirements: <br>
+                        • Relevant education in Marketing, Communications, or a related field. <br>
+                        • Proficient in...
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- End retroy layout blog posts -->
+@endSection
