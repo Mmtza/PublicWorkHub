678e78664cc98a9465cb3322f5f965607522bf8a
Anuraaaa <anwarfauzan75@gmail.com>
2023-12-06 04:10:26 +0700

    backend/admin/dashboard: add pengaduan, loker, user chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,21 +7,49 @@ use App\Models\Loker;
 use App\Models\Berita;
 use App\Models\Pengaduan;
 use App\Charts\BeritaChart;
+use App\Charts\LokerChart;
+use App\Charts\PengaduanChart;
+use App\Charts\UserChart;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
-    public function readDataDashboardAdmin(Request $request, BeritaChart $beritaChart)
+    public function readDataDashboardAdmin(
+        Request $request, BeritaChart $beritaChart, PengaduanChart $pengaduanChart, 
+        LokerChart $lokerChart, UserChart $userChart
+    )
     {
         $berita = Berita::count();
         $beritaMenunggu = Berita::where('status', 'menunggu')->count();
         $beritaAktif = Berita::where('status', 'aktif')->count();
         $beritaTidakAktif = Berita::where('status', 'tidak aktif')->count();
+        $percentageBeritaMenunggu = number_format((($beritaMenunggu / $berita) * 100), 2);
+        $percentageBeritaAktif = number_format((($beritaAktif / $berita) * 100), 2);
+        $percentageBeritaTidakAktif = number_format((($beritaTidakAktif / $berita) * 100), 2);
         $pengaduan = Pengaduan::count();
+        $pengaduanMenunggu = Pengaduan::where('status', 'menunggu')->count();
+        $pengaduanDiterima = Pengaduan::where('status', 'diterima')->count();
+        $pengaduanDitolak = Pengaduan::where('status', 'ditolak')->count();
+        $percentagePengaduanMenunggu = number_format((($pengaduanMenunggu / $pengaduan) * 100), 2);
+        $percentagePengaduanDiterima = number_format((($pengaduanDiterima / $pengaduan) * 100), 2);
+        $percentagePengaduanDitolak = number_format((($pengaduanDitolak / $pengaduan) * 100), 2);
         $loker = Loker::count();
         $user  = User::count();
-        $chart = $beritaChart->build();
-        return view('admins.index', compact('chart', 'berita', 'beritaMenunggu', 'beritaAktif', 'beritaTidakAktif', 'pengaduan', 'loker', 'user'));        
+        $chartBerita = $beritaChart->buildPC();
+        $chartBeritaMobile = $beritaChart->buildMobile();
+        $chartPengaduan = $pengaduanChart->buildPC();
+        $chartPengaduanMobile = $pengaduanChart->buildMobile();
+        $chartLoker = $lokerChart->buildPC();
+        $chartLokerMobile = $lokerChart->buildMobile();
+        $chartUser = $userChart->buildPC();
+        $chartUserMobile = $userChart->buildMobile();
+        return view('admins.index', compact(
+            'chartBerita', 'chartBeritaMobile', 'chartPengaduan', 
+            'chartPengaduanMobile', 'chartLoker', 'chartLokerMobile', 'chartUser', 'chartUserMobile',
+            'berita', 'percentageBeritaMenunggu', 'percentageBeritaAktif', 'percentageBeritaTidakAktif', 
+            'pengaduan', 'percentagePengaduanMenunggu', 'percentagePengaduanDiterima', 'percentagePengaduanDitolak', 
+            'loker', 
+            'user'));        
     }
 }
