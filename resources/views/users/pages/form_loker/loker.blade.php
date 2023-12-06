066e265b7b738d2c7201a532dbc92ac4f537bd88
Anuraaaa <anwarfauzan75@gmail.com>
2023-12-07 00:23:27 +0700

    frontend/user/loker: consume data

--- resources/views/users/pages/form_loker/loker.blade.php
+++ resources/views/users/pages/form_loker/loker.blade.php
@@ -11,105 +11,35 @@
                     <h3 class="text-dark mt-3 ms-4"><b>Daftar Loker</b></h3>
                     <div class="overflow-auto border border-primary border-1 rounded-3" style="max-height: 755px">
                         <ul class="list-unstyled">
-                            <li class="mb-2 p-2 border-bottom border-primary border-1 custom-card-hover">
-                                <a href="" class="text-decoration-none">
-                                    <div class="d-flex">
-                                        <img width="90px" height="90px" src="{{ asset('users/themes') }}/images/pwhlogo3.png" alt="">
-                                        <div class="w-16">
-                                            <h5 class="">Frontend Web Development</h5>
-                                            <span><i class="fa-solid fa-building"></i> Pt . Public Work Hub</span><br>
-                                            <span ><i class="fa-solid fa-location-dot"></i> Jl.Jendral Ahmad Yani</span><br>
-                                            <Span>11-12-2023</Span>
+                            @php
+                                $no = 0;   
+                            @endphp
+                            @foreach ($loker_side as $lokerside)                                
+                                <li class="mb-2 p-2 border-bottom border-primary border-1 custom-card-hover">
+                                    <a href="" class="text-decoration-none">
+                                        <div class="d-flex">
+                                            <img width="90px" height="90px" src="{{ asset('assets/users') }}/images/pwhlogo3.png" alt="">
+                                            <div class="w-16">
+                                                <h5 class="">{{ $lokerside->nama_loker }}</h5>
+                                                <span><i class="fa-solid fa-building"></i> {{ $lokerPublisherSide[$no] }}</span><br>
+                                                <span ><i class="fa-solid fa-location-dot"></i> {{ $lokerside->alamat }}</span><br>
+                                                <span>{{ \Carbon\Carbon::parse($lokerside->waktu_publikasi)->locale('id')->isoFormat('DD-MMMM-YYYY') }}</span>
+                                            </div>
                                         </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="mb-2 p-2 border-bottom border-primary border-1 custom-card-hover">
-                                <a href="" class="text-decoration-none">
-                                    <div class="d-flex">
-                                        <img width="90px" height="90px" src="{{ asset('users/themes') }}/images/pwhlogo3.png" alt="">
-                                        <div class="w-16">
-                                            <h5 class="">Frontend Web Development</h5>
-                                            <span><i class="fa-solid fa-building"></i> Pt . Public Work Hub</span><br>
-                                            <span ><i class="fa-solid fa-location-dot"></i> Jl.Jendral Ahmad Yani</span><br>
-                                            <Span>11-12-2023</Span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="mb-2 p-2 border-bottom border-primary border-1 custom-card-hover">
-                                <a href="" class="text-decoration-none">
-                                    <div class="d-flex">
-                                        <img width="90px" height="90px" src="{{ asset('users/themes') }}/images/pwhlogo3.png" alt="">
-                                        <div class="w-16">
-                                            <h5 class="">Frontend Web Development</h5>
-                                            <span><i class="fa-solid fa-building"></i> Pt . Public Work Hub</span><br>
-                                            <span ><i class="fa-solid fa-location-dot"></i> Jl.Jendral Ahmad Yani</span><br>
-                                            <Span>11-12-2023</Span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="mb-2 p-2 border-bottom border-primary border-1 custom-card-hover">
-                                <a href="" class="text-decoration-none">
-                                    <div class="d-flex">
-                                        <img width="90px" height="90px" src="{{ asset('users/themes') }}/images/pwhlogo3.png" alt="">
-                                        <div class="w-16">
-                                            <h5 class="">Frontend Web Development</h5>
-                                            <span><i class="fa-solid fa-building"></i> Pt . Public Work Hub</span><br>
-                                            <span ><i class="fa-solid fa-location-dot"></i> Jl.Jendral Ahmad Yani</span><br>
-                                            <Span>11-12-2023</Span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="mb-2 p-2 border-bottom border-primary border-1 custom-card-hover">
-                                <a href="" class="text-decoration-none">
-                                    <div class="d-flex">
-                                        <img width="90px" height="90px" src="{{ asset('users/themes') }}/images/pwhlogo3.png" alt="">
-                                        <div class="w-16">
-                                            <h5 class="">Frontend Web Development</h5>
-                                            <span><i class="fa-solid fa-building"></i> Pt . Public Work Hub</span><br>
-                                            <span ><i class="fa-solid fa-location-dot"></i> Jl.Jendral Ahmad Yani</span><br>
-                                            <Span>11-12-2023</Span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="mb-2 p-2 border-bottom border-primary border-1 custom-card-hover">
-                                <a href="" class="text-decoration-none">
-                                    <div class="d-flex">
-                                        <img width="90px" height="90px" src="{{ asset('users/themes') }}/images/pwhlogo3.png" alt="">
-                                        <div class="w-16">
-                                            <h5 class="">Frontend Web Development</h5>
-                                            <span><i class="fa-solid fa-building"></i> Pt . Public Work Hub</span><br>
-                                            <span ><i class="fa-solid fa-location-dot"></i> Jl.Jendral Ahmad Yani</span><br>
-                                            <Span>11-12-2023</Span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="mb-2 p-2 border-bottom border-primary border-1 custom-card-hover">
-                                <a href="" class="text-decoration-none">
-                                    <div class="d-flex">
-                                        <img width="90px" height="90px" src="{{ asset('users/themes') }}/images/pwhlogo3.png" alt="">
-                                        <div class="w-16">
-                                            <h5 class="">Frontend Web Development</h5>
-                                            <span><i class="fa-solid fa-building"></i> Pt . Public Work Hub</span><br>
-                                            <span ><i class="fa-solid fa-location-dot"></i> Jl.Jendral Ahmad Yani</span><br>
-                                            <Span>11-12-2023</Span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
+                                    </a>
+                                </li>
+                                @php
+                                    $no++;
+                                @endphp
+                            @endforeach
                         </ul>
                     </div>
                 </div>
                 <div class="col-md-6 order-first order-lg-last mt-lg-4">
-                    <h2 class="text-dark mb-3"><b>Frontend Web Development</b></h2>
+                    <h2 class="text-dark mb-3"><b>{{ $loker->nama_loker }}</b></h2>
                     <div class="row">
-                        <span class="d-flex gap-2"><i class="fa-solid fa-location-dot"></i><h6>Jl.Jendral Ahmad Yani</h6></span>
-                        <span class="d-flex gap-2"><i class="fa-solid fa-building"></i><h6>Pt . Public Work Hub</h6></span>
+                        <span class="d-flex gap-2"><i class="fa-solid fa-location-dot"></i><h6>{{ $loker->alamat }}</h6></span>
+                        <span class="d-flex gap-2"><i class="fa-solid fa-building"></i><h6>{{ $lokerPublisher }}</h6></span>
                     </div>
                     <hr>
                     <div class="d-flex mb-3 gap-4">
@@ -118,28 +48,7 @@
                         <a href="" class="btn btn-outline-primary rounded">Hubungi</a>
                     </div>
                     <div class="berita-isi-wrapper">
-                        Social Media Admin <br>
-
-                        <br>
-                        Placement in Alam Sutera (On-site) <br>
-
-                        <br>
-                        About Super <br>
-                        <br>
-
-                        We are a group of business enthusiasts, scientists, communicators, designers, productive mothers, and many more who are unified and unduly spirited to work towards one big vision and goal. As a leading social commerce platform in Indonesia, we aim to solve economic inequality distribution across rural areas by building agent networks and hyperlocal supply chains to make goods more affordable. Aplikasi Super goes beyond passion and creativity to decipher social issues in our surroundings. <br>
-
-                        <br>
-                        Key Responsibilities: <br>
-                        • Develop creative and engaging content for social media platforms. <br>
-                        • Schedule and publish content on various social media channels. <br>
-                        • Interact with the audience, respond to comments, and foster community engagement. <br>
-                        • Monitor social media analytics, track performance, and provide reports on key metrics. <br>
-
-                        <br>
-                        Role Requirements: <br>
-                        • Relevant education in Marketing, Communications, or a related field. <br>
-                        • Proficient in...
+                        {{ $loker->deskripsi_loker }}
                     </div>
                 </div>
                 <div class="col-md-3 order-lg-last order-lg-first">
@@ -154,7 +63,7 @@
                                             <h5 class="">Frontend Web Development</h5>
                                             <span><i class="fa-solid fa-building"></i> Pt . Public Work Hub</span><br>
                                             <span ><i class="fa-solid fa-location-dot"></i> Jl.Jendral Ahmad Yani</span><br>
-                                            <Span>11-12-2023</Span>
+                                            <span>11-12-2023</span>
                                         </div>
                                     </div>
                                 </a>
@@ -167,7 +76,7 @@
                                             <h5 class="">Frontend Web Development</h5>
                                             <span><i class="fa-solid fa-building"></i> Pt . Public Work Hub</span><br>
                                             <span ><i class="fa-solid fa-location-dot"></i> Jl.Jendral Ahmad Yani</span><br>
-                                            <Span>11-12-2023</Span>
+                                            <span>11-12-2023</span>
                                         </div>
                                     </div>
                                 </a>
@@ -180,7 +89,7 @@
                                             <h5 class="">Frontend Web Development</h5>
                                             <span><i class="fa-solid fa-building"></i> Pt . Public Work Hub</span><br>
                                             <span ><i class="fa-solid fa-location-dot"></i> Jl.Jendral Ahmad Yani</span><br>
-                                            <Span>11-12-2023</Span>
+                                            <span>11-12-2023</span>
                                         </div>
                                     </div>
                                 </a>
@@ -193,7 +102,7 @@
                                             <h5 class="">Frontend Web Development</h5>
                                             <span><i class="fa-solid fa-building"></i> Pt . Public Work Hub</span><br>
                                             <span ><i class="fa-solid fa-location-dot"></i> Jl.Jendral Ahmad Yani</span><br>
-                                            <Span>11-12-2023</Span>
+                                            <span>11-12-2023</span>
                                         </div>
                                     </div>
                                 </a>
@@ -206,7 +115,7 @@
                                             <h5 class="">Frontend Web Development</h5>
                                             <span><i class="fa-solid fa-building"></i> Pt . Public Work Hub</span><br>
                                             <span ><i class="fa-solid fa-location-dot"></i> Jl.Jendral Ahmad Yani</span><br>
-                                            <Span>11-12-2023</Span>
+                                            <span>11-12-2023</span>
                                         </div>
                                     </div>
                                 </a>
@@ -219,7 +128,7 @@
                                             <h5 class="">Frontend Web Development</h5>
                                             <span><i class="fa-solid fa-building"></i> Pt . Public Work Hub</span><br>
                                             <span ><i class="fa-solid fa-location-dot"></i> Jl.Jendral Ahmad Yani</span><br>
-                                            <Span>11-12-2023</Span>
+                                            <span>11-12-2023</span>
                                         </div>
                                     </div>
                                 </a>
@@ -232,7 +141,7 @@
                                             <h5 class="">Frontend Web Development</h5>
                                             <span><i class="fa-solid fa-building"></i> Pt . Public Work Hub</span><br>
                                             <span ><i class="fa-solid fa-location-dot"></i> Jl.Jendral Ahmad Yani</span><br>
-                                            <Span>11-12-2023</Span>
+                                            <span>11-12-2023</span>
                                         </div>
                                     </div>
                                 </a>
