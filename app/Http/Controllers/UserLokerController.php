c8f2a546f1f5cb34fdc11d11ad8f4b3ecc2918e6
Anuraaaa <anwarfauzan75@gmail.com>
2023-12-07 02:45:34 +0700

    backend/user/loker: finishing get data loker and pass to frontend

--- app/Http/Controllers/UserLokerController.php
+++ app/Http/Controllers/UserLokerController.php
@@ -37,27 +37,100 @@ class UserLokerController extends Controller
         $lokerPublisherSide = [];
         foreach ($loker_side as $lokerside)
         {
-            $loker_user = User::where('id', $lokerside->id_user)->first()->name;
-            $lokerPublisherSide[] = $loker_user;
+            $lokerPublisherSide[] = User::where('id', $lokerside->id_user)->first();
         }
         $loker_applier = [];
+        $user_loker_applier = [];
+        $user_loker = [];
+        $user_apply_loker = null;
         if (Auth::check()) 
         {
-            $loker_applier = Apply_Loker::where('id_user', Auth::user()->id)->get();
+            $user_loker_applier = Apply_Loker::where('id_user', Auth::user()->id)->get();
+            
+            $no_loker_applier = 0;
+            foreach ($user_loker_applier as $userapply)
+            {
+                $loker_applier[] = Loker::where('id', $userapply->id_loker)->first();
+                $user_loker[] = User::where('id', $loker_applier[$no_loker_applier]->id_user)->first();
+                $no_loker_applier++;
+            }                                    
+            $user_apply_loker = Apply_Loker::where('id_loker', $loker->id)->where('id_user', Auth::user()->id)->first();
         }
-        return view('users.pages.form_loker.loker', compact('loker', 'lokerPublisher', 'loker_side', 'lokerPublisherSide', 'loker_applier'));
+        confirmDelete();
+        return view('users.pages.form_loker.loker', compact(
+            'loker', 'lokerPublisher', 'loker_side', 'lokerPublisherSide', 
+            'loker_applier', 'user_apply_loker', 'user_loker_applier', 'user_loker'
+        ));
     }
 
 
     public function showLokerBySlug($slug)
     {
         $loker = Loker::findSlug($slug)->first();
+        $lokerPublisher = User::where('id', $loker->id_user)->first()->name;
         $loker_side = Loker::orderBy('id', 'desc')->where('id', '!=', $loker->id)->get();
+        $lokerPublisherSide = [];
+        foreach ($loker_side as $lokerside)
+        {
+            $lokerPublisherSide[] = User::where('id', $lokerside->id_user)->first();
+        }
         $loker_applier = [];
+        $user_loker_applier = [];
+        $user_loker = [];
+        $user_apply_loker = null;
         if (Auth::check()) 
         {
-            $loker_applier = Apply_Loker::where('id_user', Auth::user()->id)->get();
+            $user_loker_applier = Apply_Loker::where('id_user', Auth::user()->id)->get();
+            
+            $no_loker_applier = 0;
+            foreach ($user_loker_applier as $userapply)
+            {
+                $loker_applier[] = Loker::where('id', $userapply->id_loker)->first();
+                $user_loker[] = User::where('id', $loker_applier[$no_loker_applier]->id_user)->first();
+                $no_loker_applier++;
+            }                                    
+            $user_apply_loker = Apply_Loker::where('id_loker', $loker->id)->where('id_user', Auth::user()->id)->first();
         }
-        return view('users.pages.form_loker.loker', compact('loker', 'loker_side', 'loker_applier'));
+        confirmDelete();
+        return view('users.pages.form_loker.loker', compact(
+            'loker', 'lokerPublisher', 'loker_side', 'lokerPublisherSide', 
+            'loker_applier', 'user_apply_loker', 'user_loker_applier', 'user_loker'
+        ));
+    }
+
+    public function applyLoker($slug)
+    {
+        if (Auth::check())
+        {
+            $loker = Loker::findSlug($slug)->first();
+            Apply_Loker::insertGetId([
+                'id_user' => Auth::user()->id,
+                'id_loker' => $loker->id,
+                'status' => 'menunggu',
+                'waktu_apply' => now()
+            ]);
+            alert('Notifikasi', 'Berhasil mendaftar di lowongan pekerjaan', 'success');
+            return redirect()->back();
+        }
+        else
+        {
+            return redirect()->route('login');
+        }
+    }
+    
+    public function cancelApplyLoker($slug)
+    {        
+        if (Auth::check())
+        {
+            $loker = Loker::findSlug($slug)->first();
+            $applyLoker = Apply_Loker::where('id_user', Auth::user()->id)->where('id_loker', $loker->id)->first();
+            Apply_Loker::destroy($applyLoker->id);
+            alert('Notifikasi', 'Kamu batal mendaftar di lowongan pekerjaan', 'success');
+            return redirect()->back();
+        }
+        else
+        {
+            return redirect()->route('login');
+        }
     }
 }
