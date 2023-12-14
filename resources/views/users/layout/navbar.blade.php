b281787f5a51da78858310569419e578c7a35721
Anuraaaa <anwarfauzan75@gmail.com>
2023-12-14 13:53:56 +0700

    frontend/navbar: change logic displayed avatar

--- resources/views/users/layout/navbar.blade.php
+++ resources/views/users/layout/navbar.blade.php
@@ -81,98 +81,98 @@
                         <div class="d-none d-lg-flex flex-row-reverse justify-center align-items-center gap-5">
                             <div class="d-none d-lg-flex gap-3">
                                 @auth
-                                    @if (Auth::user()->role == 'user')
-                                        <div class="dropdown">
-                                            <button class="btn btn-none lh-1 pe-0 dropdown-toggle" type="button"
-                                                role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                                <div class="avatar avatar-l mt-2">
-                                                    @if (empty(Auth::user()->foto))
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="50"
-                                                            height="50" viewBox="0 0 24 24" fill="none"
-                                                            stroke="#ffffff" stroke-width="1" stroke-linecap="round"
-                                                            stroke-linejoin="round">
-                                                            <path
-                                                                d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3" />
-                                                            <circle cx="12" cy="10" r="3" />
-                                                            <circle cx="12" cy="12" r="10" />
-                                                        </svg>
-                                                    @else
-                                                        <img class="rounded-circle"
-                                                            src="{{ asset('assets/users/images/' . Auth::user()->foto) }}"
-                                                            alt="" />
-                                                    @endif
-                                                </div>
-                                            </button>
-                                            <ul class="dropdown-menu dropdown-menu-end px-2">
-                                                <div class="card position-relative border-0">
-                                                    <div class="card-body p-0">
-                                                        <div class="text-center pt-4 pb-3">
-                                                            <div class="avatar avatar-xl">
-                                                                @if (empty(Auth::user()->foto))
-                                                                    <div class ="rounded-circle">
-                                                                        <svg xmlns="http://www.w3.org/2000/svg"
-                                                                            width="50" height="50"
-                                                                            viewBox="0 0 24 24" fill="none"
-                                                                            stroke="#000000" stroke-width="2"
-                                                                            stroke-linecap="round" stroke-linejoin="round">
-                                                                            <path
-                                                                                d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3" />
-                                                                            <circle cx="12" cy="10" r="3" />
-                                                                            <circle cx="12" cy="12" r="10" />
-                                                                        </svg>
-                                                                    </div>
-                                                                @else
-                                                                    <img class="rounded-circle"
-                                                                        src="{{ asset('assets/users/images/' . Auth::user()->foto) }}"
-                                                                        alt="" />
-                                                                @endif
-                                                            </div>
-                                                            <h6 class="mt-2 text-black">{{ Auth::user()->name }}</h6>
+                                    <div class="dropdown">
+                                        <button class="btn btn-none lh-1 pe-0 dropdown-toggle" type="button"
+                                            role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            <div class="avatar avatar-l">
+                                                @if (empty(Auth::user()->foto))
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="50"
+                                                        height="50" viewBox="0 0 24 24" fill="none"
+                                                        stroke="#ffffff" stroke-width="1" stroke-linecap="round"
+                                                        stroke-linejoin="round">
+                                                        <path
+                                                            d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3" />
+                                                        <circle cx="12" cy="10" r="3" />
+                                                        <circle cx="12" cy="12" r="10" />
+                                                    </svg>
+                                                @else
+                                                    <img class="rounded-circle"
+                                                        src="{{ asset('assets/users/images/' . Auth::user()->foto) }}"
+                                                        alt="" />
+                                                @endif
+                                            </div>
+                                        </button>
+                                        <ul class="dropdown-menu dropdown-menu-end px-2">
+                                            <div class="card position-relative border-0">
+                                                <div class="card-body p-0">
+                                                    <div class="text-center pt-4 pb-3">
+                                                        <div class="avatar avatar-xl">
+                                                            @if (empty(Auth::user()->foto))
+                                                                <div class ="rounded-circle">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                                                        width="50" height="50"
+                                                                        viewBox="0 0 24 24" fill="none"
+                                                                        stroke="#000000" stroke-width="2"
+                                                                        stroke-linecap="round" stroke-linejoin="round">
+                                                                        <path
+                                                                            d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3" />
+                                                                        <circle cx="12" cy="10" r="3" />
+                                                                        <circle cx="12" cy="12" r="10" />
+                                                                    </svg>
+                                                                </div>
+                                                            @else
+                                                                <img class="rounded-circle"
+                                                                    src="{{ asset('assets/users/images/' . Auth::user()->foto) }}"
+                                                                    alt="" />
+                                                            @endif
                                                         </div>
-                                                        <div class="overflow-auto scrollbar">
-                                                            <ul class="nav d-flex flex-column">
+                                                        <h6 class="mt-2 text-black">{{ Auth::user()->name }}</h6>
+                                                    </div>
+                                                    <div class="overflow-auto scrollbar">
+                                                        <ul class="nav d-flex flex-column">
+                                                            <li class="nav-item">
+                                                                <a class="nav-link px-3"
+                                                                    href="{{ route('profile.edit') }}">Profile</a>
+                                                            </li>
+                                                            @if (Auth::user()->role != 'user')                                                                
                                                                 <li class="nav-item">
-                                                                    <a class="nav-link px-3"
-                                                                        href="{{ route('profile.edit') }}">Profile</a>
+                                                                    @if (Auth::user()->role == 'admin')
+                                                                        @if (Route::has('admin'))
+                                                                            <a href="{{ route('admin') }}" class="nav-link px-3">Dashboard</a>
+                                                                        @endif
+                                                                    @elseif (Auth::user()->role == 'penulis')
+                                                                        @if (Route::has('penulis'))
+                                                                            <a href="{{ route('penulis') }}" class="nav-link px-3">Dashboard</a>
+                                                                        @endif
+                                                                    @elseif (Auth::user()->role == 'penyedia_loker')
+                                                                        @if (Route::has('penyedia-loker'))
+                                                                            <a href="{{ route('penyedia-loker') }}" class="nav-link px-3">Dashboard</a>
+                                                                        @endif
+                                                                    @endif
                                                                 </li>
-                                                                <li class="nav-item">
-                                                                    <a class="nav-link px-3"
-                                                                        href="{{ route('loker/chat') }}">Chat</a>
-                                                                </li>
-                                                                <li class="nav-item">
-                                                                    <a class="nav-link px-3"
-                                                                        href="{{ route('users.pengaduan') }}">Pengaduan</a>
-                                                                </li>
-                                                                <li class="nav-item">
-                                                                    <form class="bg-transparent"
-                                                                        action="{{ route('logout') }}" method="POST">
-                                                                        @csrf
-                                                                        <button type="submit"
-                                                                            class="btn btn-none px-3">Logout</button>
-                                                                    </form>
-                                                                </li>
-                                                            </ul>
-                                                        </div>
+                                                            @endif
+                                                            <li class="nav-item">
+                                                                <a class="nav-link px-3"
+                                                                    href="{{ route('loker/chat') }}">Chat</a>
+                                                            </li>
+                                                            <li class="nav-item">
+                                                                <a class="nav-link px-3"
+                                                                    href="{{ route('users.pengaduan') }}">Pengaduan</a>
+                                                            </li>
+                                                            <li class="nav-item">
+                                                                <form class="bg-transparent"
+                                                                    action="{{ route('logout') }}" method="POST">
+                                                                    @csrf
+                                                                    <button type="submit"
+                                                                        class="btn btn-none px-3">Logout</button>
+                                                                </form>
+                                                            </li>
+                                                        </ul>
                                                     </div>
                                                 </div>
-                                            </ul>
-                                        </div>
-                                    @elseif (Auth::user()->role == 'admin')
-                                        @if (Route::has('admin'))
-                                            <a href="{{ route('admin') }}"
-                                                class="text-white btn btn-outline-secondary">Dashboard</a>
-                                        @endif
-                                    @elseif (Auth::user()->role == 'penulis')
-                                        @if (Route::has('penulis'))
-                                            <a href="{{ route('penulis') }}"
-                                                class="text-white btn btn-outline-secondary">Dashboard</a>
-                                        @endif
-                                    @elseif (Auth::user()->role == 'penyedia_loker')
-                                        @if (Route::has('penyedia-loker'))
-                                            <a href="{{ route('penyedia-loker') }}"
-                                                class="text-white btn btn-outline-secondary">Dashboard</a>
-                                        @endif
-                                    @endif
+                                            </div>
+                                        </ul>
+                                    </div>
                                 @else
                                     @if (Route::has('login'))
                                         <a href="{{ route('login') }}"
